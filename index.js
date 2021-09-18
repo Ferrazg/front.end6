82741d97bf8410002499e497f6acb953165fd59b
FE6-Guilherme Ferraz <ferrazg423@gmail.com>
2021-09-17 22:28:51 -0300

    criado calculadora de média

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+let N1 = parseFloat(window.prompt("Primeira nota"));
+let N2 = parseFloat(window.prompt("Primeira nota"));
+let N3 = parseFloat(window.prompt("Primeira nota"));
+let media = (N1+N2+N3)/3;
+ 
+    if(media >= 8) {
+        alert("Aprovado");
+    } else{
+        alert("Reprovado");
+    }
